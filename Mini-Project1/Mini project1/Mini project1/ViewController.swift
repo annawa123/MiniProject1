875137752854a24910a2e17f5c5e9f54fe6fb576
annawa123 <69251160+annawa123@users.noreply.github.com>
2020-08-06 14:57:36 -0400

    Add files via upload

---
+++ Mini-Project1/Mini project1/Mini project1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Mini project1
+//
+//  Created by Anna Waldbaum on 8/5/20.
+//  Copyright © 2020 Anna Waldbaum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
